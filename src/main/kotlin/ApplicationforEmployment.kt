170a7bdaf9df7c66a1d91b4edd53971b781cb9e6
We6336 <anp.wfh@gmail.com>
2022-10-07 20:49:06 +0800

    Initial Commit

---
+++ src/main/kotlin/ApplicationforEmployment.kt
@@ -0,0 +1,26 @@
+
+//Application for Employment
+var positionYouAreApplyingFor = ""
+var desiredSalary = ""
+var dateAvailableforWork = ""
+
+// Personal Information
+var lastName = ""
+var firstName = ""
+var middleName = ""
+var address = ""
+var city = ""
+var state = ""
+var zip = ""
+var homePhone = ""
+var cellPhone = ""
+var emailAddress = ""
+var socialSecurityNumber = ""
+var areYouAUsCitizen = false
+var isUsCitizen = true
+var haveYouEverBeenConvictedOfAFelony = false
+var isConvictedOfAFelony = true
+var ifSelectedForEmploymentAreYouWillingToSubmitToAPreEmploymentDrugScreeningScreeningTest = false
+var ifWillingToSubmitToAPreEmploymentDrugScreeningScreeningTest = true
+
+//
